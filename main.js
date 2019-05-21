478afaf901a221a545233fa3f33cc5f32d82164f
Jordan <jordanmittelman@gmail.com>
2019-05-21 12:32:25 -0400

    added some pseudocode

--- main.js
+++ main.js
@@ -1,1 +1,96 @@
 console.log("Check me out!");
+
+// ----------------Data
+
+// create object with 10 questions + 2 wrong answers and
+// 1 correct answer for each
+
+var trivia = {
+  question1: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question2: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question3: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question4: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question4: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question5: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question6: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question7: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question8: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question9: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  },
+  question10: {
+    question: "",
+    correctAnswer: "",
+    wrongAnswer1: "",
+    wrongAnswer2: ""
+  }
+};
+// --------------functinality
+
+// show modal box with "Ready?/Get Started" button
+
+// use event listener for click on button
+
+// remove modal and then provide question 1 by adding question text
+// to the question box and answer text to the answer boxes
+
+// use event listener for click on selection button
+
+// then show result and "next" button  in result box and update score
+// score box
+
+// clear question box and answer boxes
+
+// then provide question 2
+
+// continue for total 10 times
+
+// show modal box with final score and ask to play again
